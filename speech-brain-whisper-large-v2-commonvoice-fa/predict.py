481e6cabc74b11cd89a8175c6aab1a619aa760cc
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-12-15 21:36:25 +0330

    feat: add speechbrain asr-whisper-large-v2-commonvoice-fa model

---
+++ speech-brain-whisper-large-v2-commonvoice-fa/predict.py
@@ -0,0 +1,30 @@
+import torch
+import torchaudio
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+
+# Set the device to GPU if available, otherwise use CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+def load_model():
+    model_name = "speechbrain/asr-whisper-large-v2-commonvoice-fa"
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(model_name, torch_dtype=torch_dtype)
+    model.to(device)
+    processor = AutoProcessor.from_pretrained(model_name)
+    return processor, model
+
+def predict(processor, model, audio_path):
+    # Initialize the pipeline
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        torch_dtype=torch_dtype,
+        device=device,
+        return_timestamps=True,
+    )
+
+    result = pipe(audio_path)
+    transcription = result["text"]
+    return transcription
