a5d9e9a52a71e460309829af7d2af1fe212932d8
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2024-12-16 21:20:01 +0330

    Refactor variable name in load_model function

--- speech-brain-asr-whisper-large-v2-commonvoice-fa/predict.py
+++ speech-brain-asr-whisper-large-v2-commonvoice-fa/predict.py
@@ -1,8 +1,8 @@
 from speechbrain.inference.ASR import WhisperASR
 
 def load_model():
-    asr_model_name = "speechbrain/asr-whisper-large-v2-commonvoice-fa"
-    asr_model = WhisperASR.from_hparams(source=asr_model_name, savedir="pretrained_models/model")
+    model_name = "speechbrain/asr-whisper-large-v2-commonvoice-fa"
+    asr_model = WhisperASR.from_hparams(source=model_name, savedir="pretrained_models/model")
     return asr_model
 
 def predict(asr_model, audio_path):
